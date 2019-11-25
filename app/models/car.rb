3e0eec620f02dc909aad5a4d72a1f7e25bfea5f2
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-25 14:08:41 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,39 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
 
+  @@all = []
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    @car_owner
+    @mechanic
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    @@all.map {|cars| cars.classification}.uniq
+  end
+
+  def mechanics
+    Mechanic.all.select {|mechanics| mechanics.specialty == self.classification}
+  end
+
+  def new_owner(owner)
+    # car gets a new owner
+    @car_owner = owner
+  end
+
+  def new_mechanic(mechanic)
+    # adds a mechanic to the car
+    @mechanic = mechanic
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,33 @@ def reload
   load 'config/environment.rb'
 end
 
+shakil = CarOwner.new("Shakil")
+alex = CarOwner.new("Alex")
+colin = CarOwner.new("Colin")
+gianca = CarOwner.new("Giancarlo")
+
+benz = Car.new("Mercedes", "E300", "luxury")
+prius = Car.new("Toyota", "Prius", "electric")
+tesla = Car.new("Tesla", "Model 3", "electric")
+gt = Car.new("Ford", "GT", "antique")
+beamer = Car.new("BMW", "M5", "exotic")
+
+tony = Mechanic.new("Tony", "luxury")
+elon = Mechanic.new("Elon", "electric")
+buddy = Mechanic.new("Buddy", "antique")
+richard = Mechanic.new("Richard", "luxury")
+james = Mechanic.new("James", "exotic")
+
+benz.new_owner(shakil)
+prius.new_owner(colin)
+tesla.new_owner(shakil)
+gt.new_owner(colin)
+beamer.new_owner(gianca)
+
+benz.new_mechanic(tony)
+gt.new_mechanic(buddy)
+tesla.new_mechanic(elon)
+prius.new_mechanic(elon)
+beamer.new_mechanic(james)
+
 binding.pry
