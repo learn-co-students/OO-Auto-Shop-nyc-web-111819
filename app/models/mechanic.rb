1aaa75ca481218f9d0fbcfc7bbca20522dff02e9
Miss-Cheese <nat.galligan@gmail.com>
2019-11-25 20:13:59 -0500

    Done.

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,34 @@ class Mechanic
 
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  @@all = []
 
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def mech_car_list
+    Car.all.select {|car|
+    car.mechanic == self
+    }
+  end
+
+  def car_owner_list
+    mech_car_list.map {|car|
+    car.owner
+    }
+  end
+
+  def car_owner_names
+    car_owner_list.map {|owner|
+    owner.name
+    }
+  end
 
 end
